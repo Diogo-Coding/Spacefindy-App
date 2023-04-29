b1474de5558e310cb9b191812a83ba78b2a0a8b9
Diogo-Coding <diogo.hcp.2001@gmail.com>
2023-04-29 19:35:46 +0100

    0.0.1

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -1,13 +1,55 @@
 <template>
   <div class="home">
-    HOME VIEW
+    <div class="navBar">
+      <div class="logo">
+        <img src="" alt="">
+      </div>
+      <div class="buttonsNav">
+        <button class="AboutNavButton navButton">Acerca de nosotros</button>
+        <button class="LogInNavButton navButton">Log In</button>
+      </div>
+      <div class="buttonSearchNav">
+        <button class="SearchNavButton navButton"><i class="fa-solid fa-magnifying-glass fa-xl"></i></button>
+      </div>
+    </div>
+    <div class="mainPage">
+
+    </div>
   </div>
 </template>
 
 <script>
+import { onMounted } from 'vue';
+import { loadScript } from "vue-plugin-load-script";
+
 export default {
   name: 'HomeView',
   components: {
+  },
+  setup() {
+    
+
+    onMounted(() => {
+      loadScript("https://kit.fontawesome.com/771394cdc2.js")
+    })
   }
 }
 </script>
+
+<style scoped>
+  .home {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+  }
+  .navBar {
+    display: flex;
+    justify-content: space-between;
+    padding: 1% 3% 1% 3%;
+    background-color: gainsboro;
+  }
+  .navButton {
+
+  }
+</style>
