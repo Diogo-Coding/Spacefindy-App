c27336b9ebaffb58d7a1bc2e6add913ba741ba16
Diogo-Coding <diogo.hcp.2001@gmail.com>
2023-04-30 21:01:07 +0100

    https invalid cert added

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
+const fs = require('fs')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    host: 'localhost',
+    port: 8083,
+    https: {
+      key: fs.readFileSync('./localhost-key.pem'),
+      cert: fs.readFileSync('./localhost.pem'),
+    }
+  }
 })
